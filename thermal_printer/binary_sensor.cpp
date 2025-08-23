e8f463d4531649e53a94f518f3ac146b6967f854
rlatimer4 <54006308+rlatimer4@users.noreply.github.com>
2025-08-23 11:27:48 -0400

    Update binary_sensor.cpp

--- thermal_printer/binary_sensor.cpp
+++ thermal_printer/binary_sensor.cpp
@@ -7,19 +7,36 @@ namespace thermal_printer {
 static const char *const TAG = "thermal_printer.binary_sensor";
 
 void ThermalPrinterBinarySensor::setup() {
-  this->parent_->set_paper_check_callback([this](bool has_paper) {
+  // Set up callback for paper status changes
+  if (this->parent_) {
+    this->parent_->set_paper_check_callback([this](bool has_paper) {
+      if (this->state != has_paper) {
+        this->publish_state(has_paper);
+        ESP_LOGD(TAG, "Paper loaded: %s", has_paper ? "YES" : "NO");
+      }
+    });
+  }
+  
+  // Initial status check
+  if (this->parent_) {
+    bool has_paper = this->parent_->has_paper();
+    this->last_state_ = has_paper;
     this->publish_state(has_paper);
-  });
+  }
 }
 
 void ThermalPrinterBinarySensor::loop() {
   // Check paper status every 10 seconds
   if (millis() - this->last_check_ > 10000) {
     this->last_check_ = millis();
-    bool has_paper = this->parent_->has_paper();
-    if (has_paper != this->last_state_) {
-      this->last_state_ = has_paper;
-      this->publish_state(has_paper);
+    
+    if (this->parent_) {
+      bool current_status = this->parent_->has_paper();
+      if (current_status != this->last_state_) {
+        this->last_state_ = current_status;
+        this->publish_state(current_status);
+        ESP_LOGD(TAG, "Paper status changed: %s", current_status ? "Loaded" : "Out");
+      }
     }
   }
 }
