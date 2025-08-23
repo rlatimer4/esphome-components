4f05a2b300e46317515e71f4fcf7d22fe5226344
rlatimer4 <54006308+rlatimer4@users.noreply.github.com>
2025-08-23 15:30:18 -0400

    Update binary_sensor.cpp

--- components/thermal_printer/binary_sensor.cpp
+++ components/thermal_printer/binary_sensor.cpp
@@ -1,18 +1,19 @@
-#include "binary_sensor.h"
+#include "text_sensor.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
 namespace thermal_printer {
 
-static const char *const TAG = "thermal_printer.binary_sensor";
+static const char *const TEXT_SENSOR_TAG = "thermal_printer.text_sensor";
 
-void ThermalPrinterBinarySensor::setup() {
+void ThermalPrinterTextSensor::setup() {
   // Set up callback for paper status changes
   if (this->parent_) {
     this->parent_->set_paper_check_callback([this](bool has_paper) {
-      if (this->state != has_paper) {
-        this->publish_state(has_paper);
-        ESP_LOGD(TAG, "Paper loaded: %s", has_paper ? "YES" : "NO");
+      std::string status = has_paper ? "Present" : "Out";
+      if (this->state != status) {
+        this->publish_state(status);
+        ESP_LOGD(TEXT_SENSOR_TAG, "Paper status: %s", status.c_str());
       }
     });
   }
@@ -20,22 +21,23 @@ void ThermalPrinterBinarySensor::setup() {
   // Initial status check
   if (this->parent_) {
     bool has_paper = this->parent_->has_paper();
-    this->last_state_ = has_paper;
-    this->publish_state(has_paper);
+    this->last_paper_status_ = has_paper;
+    this->publish_state(has_paper ? "Present" : "Out");
   }
 }
 
-void ThermalPrinterBinarySensor::loop() {
+void ThermalPrinterTextSensor::loop() {
   // Check paper status every 10 seconds
   if (millis() - this->last_check_ > 10000) {
     this->last_check_ = millis();
     
     if (this->parent_) {
       bool current_status = this->parent_->has_paper();
-      if (current_status != this->last_state_) {
-        this->last_state_ = current_status;
-        this->publish_state(current_status);
-        ESP_LOGD(TAG, "Paper status changed: %s", current_status ? "Loaded" : "Out");
+      if (current_status != this->last_paper_status_) {
+        this->last_paper_status_ = current_status;
+        std::string status = current_status ? "Present" : "Out";
+        this->publish_state(status);
+        ESP_LOGD(TEXT_SENSOR_TAG, "Paper status changed: %s", status.c_str());
       }
     }
   }
