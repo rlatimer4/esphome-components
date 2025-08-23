d1d78cddea5fdc70b803e18735944f82661402cb
rlatimer4 <54006308+rlatimer4@users.noreply.github.com>
2025-08-23 11:05:45 -0400

    Create binary_sensor.py

---
+++ thermal_printer/binary_sensor.py
@@ -0,0 +1,24 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from esphome.const import CONF_ID
+from . import thermal_printer_ns, ThermalPrinterComponent, CONF_THERMAL_PRINTER_ID
+
+DEPENDENCIES = ["thermal_printer"]
+
+ThermalPrinterBinarySensor = thermal_printer_ns.class_("ThermalPrinterBinarySensor", binary_sensor.BinarySensor, cg.Component)
+
+CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(ThermalPrinterBinarySensor),
+        cv.GenerateID(CONF_THERMAL_PRINTER_ID): cv.use_id(ThermalPrinterComponent),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await binary_sensor.register_binary_sensor(var, config)
+    await cg.register_component(var, config)
+    
+    parent = await cg.get_variable(config[CONF_THERMAL_PRINTER_ID])
+    cg.add(var.set_parent(parent))
