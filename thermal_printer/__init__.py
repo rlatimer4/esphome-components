b37499b113b80e997cb766ebeb6473f4a5fbe28e
rlatimer4 <54006308+rlatimer4@users.noreply.github.com>
2025-08-23 11:20:30 -0400

    Update __init__.py

--- thermal_printer/__init__.py
+++ thermal_printer/__init__.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import uart
 from esphome.const import CONF_ID
 
@@ -27,9 +26,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
-    
-    # Add required libraries
-    cg.add_library("Adafruit Thermal Printer Library", "1.4.3")
-    
-    # Add include for Arduino compatibility
-    cg.add_global(cg.global_ns.using("Print"))
